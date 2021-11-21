19239c77ab43099b90fc8282ba8298bc826c0754
Coder962 <76476530+Coder962@users.noreply.github.com>
2021-11-21 12:50:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -13,7 +18,13 @@ function gotPoses(results) {
     if(results.length > 0) {
         console.log("results");
     }
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 }
 function draw() {
-    background('##808080');
+    background('#fff');
+textSize(difference);
+    fill('#ff007f');
+    text('Lorem ipsum', 50, 400)
 }
